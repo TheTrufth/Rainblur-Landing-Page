7947e0d5b454e11c822263468c4cb3f3bd582c55
Jay McCaughrean <zcabmcc@ucl.ac.uk>
2022-04-04 17:15:14 +0100

    critical evaluation added

--- ns-projectsite/src/Pages/evaluation.jsx
+++ ns-projectsite/src/Pages/evaluation.jsx
@@ -280,6 +280,38 @@ const Evaluation = () => {
 
                 </div>
 
+                <div className="divider" />
+                <div tabIndex="0" className="collapse container rounded-box collapse-plus">
+                    <input type="checkbox" />
+                    <h1 className="collapse-title bg-clip-text text-transparent bg-gradient-to-bl from-sky-400 via-teal-400 to-lime-600 text-center text-2xl md:text-4xl underline-offset-8 mb-8 underline decoration-pink-600">
+                        &gt; Critical Evaluation
+                    </h1>
+
+                    <div className="collapse-content">
+                        <h2 className='text-2xl underline underline-offset-2 text-primary'>Critical Evaluation of the Project</h2>
+                        <p>We conducted an honest evaluation of the finished product and the work we put into it. The evaluation is divided into 5 segments: functionality, UI and UX, compatibility, maintainability, and project management.</p>
+                        
+                        <h2 className='text-2xl underline underline-offset-2 text-primary'>Functionality</h2>
+                        <p>We successfully transferred all existing functionality from MobileV1 to Nudge Share. Nudge Share connects seamlessly with the existing backend API endpoints of MobileV1 to complete speech analysis with IMB Watson. Indeed, 100% of “Must Have’s” from our list of requirements has been achieved. We also completed 50% of optional requirements such as the ability to change passwords. All functionality has been tested and is confirmed to be working.</p>
+                        <p>Score: 10/10</p>
+
+                        <h2 className='text-2xl underline underline-offset-2 text-primary'>UI and UX</h2>
+                        <p>We made use of a CSS framework called Tailwind which allowed us to develop a responsive and dynamic UI efficiently. We also used CSS libraries to add modern icons to make the app coherent and comprehensive. We took inspiration from Google’s material design guild lines to create a native UI and UX feel for users. However, there is still room for improvement in UI and UX. For a new user, some parts of the app may not feel as intuitive as it does to us. Therefore, further user testing is needed to make continuous improvements to the design.</p>
+                        <p>Score: 8/10</p>
+
+                        <h2 className='text-2xl underline underline-offset-2 text-primary'>Compatibility</h2>
+                        <p>Compatibility was one of the core themes for this project. MobileV1 was only available as an Android app, whereas Nudge Share is available on mobile and desktop. Components of the app not only scale according to screen size but rearrange themselves. For example, the navigation bar is stacked on the left side of the screen on a wide window but moves to the bottom of the screen for a narrow window such as on mobile. Also, since Nudge Share is written using React, it can easily be deployed on all major App stores as a PWA.</p>
+                        <p>Score: 10/10</p>
+
+                        <h2 className='text-2xl underline underline-offset-2 text-primary'>Maintainability</h2>
+                        <p>The code for Nudge Share is well documented with comments and the project maintains a coherent file structure. We adopted a modular design by splitting and re-using components and hooks in React. This makes it easier to add new components to the app without breaking existing functionality. Maintainability could be improved by including a more in-depth “read me” file for new developers. </p>
+                        <p>Score: 8/10</p>
+
+                        <h2 className='text-2xl underline underline-offset-2 text-primary'>Project Management</h2>
+                        <p>We initially struggled with project management. Each member of the team came into the project with different levels of experience, and it was difficult to coordinate efforts to complete tasks in the remote work environment. Things improved as we started implementing more detailed weekly plans for each member and worked together more often on Microsoft Teams. Although contribution varied, each member played an important role in delivering the final project.</p>
+                        <p>Score: 5/10</p>
+                    </div>
+                </div>
 
             </div>
 
